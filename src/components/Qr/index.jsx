1eeb9bb59c258e1d2e11b25c3ea6925e5a03f276
Sergey Roschin <Sergey210500@gmail.com>
2023-10-31 16:10:14 +0300

    дизайн

--- src/components/Qr/index.jsx
+++ src/components/Qr/index.jsx
@@ -1,106 +1,148 @@
-import React, { useEffect, useState } from "react";
-import QrReader from "react-qr-scanner";
-import styles from "./index.module.scss";
+import React, { useEffect, useState } from 'react'
+import QrReader from 'react-qr-scanner'
+import styles from './index.module.scss'
+import Image from 'next/image'
+import galleryIcon from '../../assets/img/gallery.svg'
+import lightningIcon from '../../assets/img/lightning.svg'
+import crossIcon from '../../assets/img/cross.svg'
 
 const Qr = () => {
-  const [data, setData] = useState({});
-  const [userData, setUserData] = useState({
-    userName: "",
-    phone: "",
-  });
-  const [devices, setDevices] = useState({
-    facingMode: "environment",
-  });
+	const [data, setData] = useState({})
+	const [userData, setUserData] = useState({
+		userName: '',
+		phone: '',
+	})
+	const [devices, setDevices] = useState({
+		facingMode: 'environment',
+	})
 
-  const handleScan = (e) => {
-    if (e?.text && !Object.keys(data || {}).length) {
-      setData(e);
-    }
-  };
+	const handleScan = e => {
+		if (e?.text && !Object.keys(data || {}).length) {
+			setData(e)
+		}
+	}
 
-  const refresh = () => {
-    setData({});
-  };
+	const refresh = () => {
+		setData({})
+	}
 
-  const handleError = (err) => {
-    console.error(err);
-  };
+	const handleError = err => {
+		console.error(err)
+	}
 
-  useEffect(() => {
-    const a = "enumerateDevices";
-    navigator["mediaDevices"]
-      [a]()
-      .then((devices) => {
-        const videoSelect = [];
-        devices.forEach((device) => {
-          if (device.kind === "videoinput") {
-            videoSelect.push(device);
-          }
-        });
-        return videoSelect;
-      })
-      .then((devices) => {
-        setDevices({
-          cameraId: devices[0].deviceId,
-          devices,
-          loading: false,
-        });
-      });
-  }, []);
+	useEffect(() => {
+		const a = 'enumerateDevices'
+		navigator['mediaDevices']
+			[a]()
+			.then(devices => {
+				const videoSelect = []
+				devices.forEach(device => {
+					if (device.kind === 'videoinput') {
+						videoSelect.push(device)
+					}
+				})
+				return videoSelect
+			})
+			.then(devices => {
+				setDevices({
+					cameraId: devices[0].deviceId,
+					devices,
+					loading: false,
+				})
+			})
+	}, [])
 
-  const changeCamera = (cameraId) => {
-    setDevices({
-      ...devices,
-      cameraId,
-    });
-  };
+	const changeCamera = cameraId => {
+		setDevices({
+			...devices,
+			cameraId,
+		})
+	}
 
-  console.log(data);
+	console.log(data)
 
-  const getUserData = (element) => {
-    setUserData({
-        ...userData,
-        userName: element
-    })
-  };
+	const getUserData = element => {
+		setUserData({
+			...userData,
+			userName: element,
+		})
+	}
 
-  return (
-    <div className={styles.container_btn}>
-      <button className={styles["btn_refresh"]} onClick={refresh}>
-        Refresh
-      </button>
-      <div className={styles["devices"]}>
-        {devices?.devices?.map((dev) => (
-          <button
-            onClick={() => changeCamera(dev.deviceId)}
-            className={styles["btn_device"]}
-          >
-            {dev.label}
-          </button>
-        ))}
-      </div>
-      {!Object.keys(data || {}).length ? (
-        <QrReader
-          className={styles["scanner"]}
-          onScan={handleScan}
-          delay={500}
-          onError={handleError}
-          constraints={
-            devices.cameraId && {
-              audio: false,
-              video: { deviceId: devices.cameraId },
-            }
-          }
-        />
-      ) : null}
+	return (
+		<div className={styles.container_btn}>
+			<div className={styles['btn_footer']}>
+				<div>
+					{/* <input
+						className={styles['input_gallery']}
+						type='file'
+						accept='image/*'
+					/> */}
+					<button type='button' className={styles['btn_gallery']}>
+						<Image
+							src={galleryIcon}
+							height={36}
+							width={36}
+							priority={false}
+							alt='Gallery'
+						/>
+					</button>
+				</div>
+				<button className={styles['btn_refresh']} onClick={refresh}>
+					Refresh
+				</button>
+				<button className={styles['btn_flashlight']}>
+					<Image
+						src={lightningIcon}
+						height={36}
+						width={36}
+						priority={false}
+						alt='Flashlight'
+					/>
+				</button>
+			</div>
 
-      {data.text ? (
-        <div className={styles.content}>
-          {/http/.test(data.text) ? <iframe src={data.text} /> : data.text}
-        </div>
-      ) : null}
-    </div>
-  );
-};
+			<div className={styles['btn_header']}>
+				<div className={styles['devices']}>
+					{devices?.devices?.map(dev => (
+						<button
+							onClick={() => changeCamera(dev.deviceId)}
+							className={styles['btn_device']}
+						>
+							{dev.label}
+						</button>
+					))}
+				</div>
+				<button className={styles['btn_cross']}>
+					<Image
+						src={crossIcon}
+						height={36}
+						width={36}
+						priority={false}
+						alt='Cross'
+					/>
+				</button>
+			</div>
+			{!Object.keys(data || {}).length ? (
+				<QrReader
+					className={styles['scanner']}
+					onScan={handleScan}
+					delay={500}
+					onError={handleError}
+					constraints={
+						devices.cameraId && {
+							audio: false,
+							video: { deviceId: devices.cameraId },
+						}
+					}
+				/>
+			) : null}
+			{data.text ? (
+				<div className={styles.content}>
+					{/http/.test(data.text) ? <iframe src={data.text} /> : data.text}
+				</div>
+			) : null}
+		</div>
+	)
+}
 
-export default Qr;
+export default Qr
